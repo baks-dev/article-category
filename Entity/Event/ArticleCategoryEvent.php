3646f5ac78918b4a7004d674c5ef8b8f05a7ca26
kingstar <admin@baks.dev>
2025-04-28 00:05:01 +0300

    Baks Development

--- Entity/Event/ArticleCategoryEvent.php
+++ Entity/Event/ArticleCategoryEvent.php
@@ -1,17 +1,17 @@
 <?php
 /*
- *  Copyright 2023.  Baks.dev <admin@baks.dev>
- *
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is furnished
  *  to do so, subject to the following conditions:
- *
+ *  
  *  The above copyright notice and this permission notice shall be included in all
  *  copies or substantial portions of the Software.
- *
+ *  
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
@@ -29,13 +29,13 @@ use BaksDev\Article\Category\Entity\ArticleCategory;
 use BaksDev\Article\Category\Entity\Modify\ArticleCategoryModify;
 use BaksDev\Article\Category\Type\Event\ArticleCategoryEventUid;
 use BaksDev\Article\Category\Type\Id\ArticleCategoryUid;
+use BaksDev\Core\Entity\EntityEvent;
+use BaksDev\Core\Entity\EntityState;
 use BaksDev\Core\Type\Locale\Locale;
 use BaksDev\Core\Type\Modify\ModifyAction;
 use Doctrine\Common\Collections\Collection;
-use Doctrine\ORM\Mapping as ORM;
 use Doctrine\DBAL\Types\Types;
-use BaksDev\Core\Entity\EntityEvent;
-use BaksDev\Core\Entity\EntityState;
+use Doctrine\ORM\Mapping as ORM;
 use InvalidArgumentException;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -46,8 +46,6 @@ use Symfony\Component\Validator\Constraints as Assert;
 #[ORM\Table(name: 'article_category_event')]
 class ArticleCategoryEvent extends EntityState
 {
-    public const TABLE = 'article_category_event';
-
     /**
      * Идентификатор События
      */
@@ -68,7 +66,7 @@ class ArticleCategoryEvent extends EntityState
     /**
      * Модификатор
      */
-    #[ORM\OneToOne(targetEntity: ArticleCategoryModify::class, mappedBy: 'event', cascade: ['all'])]
+    #[ORM\OneToOne(targetEntity: ArticleCategoryModify::class, mappedBy: 'event', cascade: ['all'], fetch: 'EAGER')]
     private ArticleCategoryModify $modify;
 
 
